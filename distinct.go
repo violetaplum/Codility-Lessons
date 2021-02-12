abf20fd817c5513aa53d64afd662ec06f342f18f
pam <pam@swit.io>
2021-02-12 20:50:36 +0900

    주석

--- distinct.go
+++ distinct.go
@@ -6,8 +6,9 @@ import (
 )
 
 func Distinct(A []int) int {
-
 	sort.Ints(A)
+	//TODO: 사실 여기서 sort는 필수요소는 아니다
+	// map이기 때문에 어차피 정렬을 하나 안하나 똑같다
 	mapmap := make(map[int]int, len(A))
 
 	for _, v := range A {
